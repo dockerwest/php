e28cda3307da9c5d183b4e9f09775155f4cf93b2
BlackEagle <ike.devolder@gmail.com>
2017-02-06 10:41:55 +0100

    xhgui documentation stated serialize was needed for import but it takes json
    Signed-off-by: BlackEagle <ike.devolder@gmail.com>

--- 7.1/scripts/usr/share/tideways/prepend.php
+++ 7.1/scripts/usr/share/tideways/prepend.php
@@ -1,14 +1,20 @@
 <?php
 
 if (extension_loaded('tideways')) {
-    tideways_enable(TIDEWAYS_FLAGS_CPU | TIDEWAYS_FLAGS_MEMORY | TIDEWAYS_FLAGS_NO_SPANS);
+    tideways_enable(
+        TIDEWAYS_FLAGS_CPU
+        | TIDEWAYS_FLAGS_MEMORY
+        | TIDEWAYS_FLAGS_NO_SPANS
+    );
 
     register_shutdown_function(
         function() {
             $data = tideways_disable();
             file_put_contents(
-                "/var/www/xhprof/" . microtime(true) . ".xhprof",
-                serialize($data)
+                "/var/www/xhprof/"
+                    . number_format(microtime(true), 4)
+                    . ".xhprof",
+                json_encode($data)
             );
         }
     );
